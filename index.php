860490ff84ceffa462ce92972cecc5313fe1336c
ChloeArd <chloe.ardoise@gmail.com>
2021-01-22 14:52:15 +0100

    Exercice 142

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$bienvenue = "Bienvenue !";
+echo $bienvenue."<br>";
+
+$array = ["Chien", "Chat", "Cheval", "Lapin"];
+echo $array[1]."<br>";
+
+for ($i = 0; $i <= 3; $i++){
+    echo $array[$i]."<br>";
+}
+?>
+
+<ul><?php foreach ($array as $all){echo $all."<br>";} ?></ul>
+
+
